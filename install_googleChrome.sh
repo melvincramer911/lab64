017a5d6e0bdb26acbb97e144c347060321c0be34
timfox123 <d@davidbr.com>
2020-06-21 09:19:28 -0400

    forgot to add my files lasttime

---
+++ install_googleChrome.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+funky(){ 
+
+trap read debug 
+
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+
+#yum install ./google-chrome-stable_current_*.rpm
+
+yum install ./google-chrome-stable_current_x86_64.rpm -y
+
+google-chrome &
+
+}
+
+
+funky
+
+
+
+
