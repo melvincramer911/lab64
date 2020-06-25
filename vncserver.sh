3acd150912f36190fa71529213425efe2ed87a84
melvincramer911 <davidbr@davidbr.com>
2020-06-25 15:30:18 -0400

    Create vncserver.sh

---
+++ vncserver.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x 
+
+trap read debug 
+
+
+
+## read this howto:  https://www.thegeekdiary.com/how-to-install-and-configure-vnc-tigervnc-server-in-centos-rhel-7/  
+## make sure you use the :3 in the service files.   if you do an nmap ot the host you will see the sevice runs in 5901!!
+
